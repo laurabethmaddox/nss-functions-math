ff8f855e0794cfb93b8c425f52bd4c2982cadb3f
Laura Beth Maddox <laurabethmaddox@gmail.com>
2022-01-17 12:08:20 -0600

    Functions math commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const addNumbers = (firstNumber, secondNumber) => {
+    const sumOfNumbers = 23 + 22
+
+    return sumOfNumbers
+}
+
+const multiplyNumbers = (firstNumber, secondNumber) => {
+    const productOfNumbers = 6 * 2
+
+    return productOfNumbers
+}
+
+const addFunction = addNumbers(23, 22)
+
+const productFunction = multiplyNumbers(6, 2)
+
+console.log(productFunction)
